b2a475494b5dad9f719c368a7c280f1abf345b4f
Rick Hewitt <Rick.Lira.Morgan@gmail.com>
2020-07-15 23:49:30 -0700

    Experimentation file for messing with new functions and such, imports all main dependencies

---
+++ experimentation.py
@@ -0,0 +1,11 @@
+"""
+
+This is a file to be used for experimentation purposes ONLY. All actual software should be packaged as such.
+
+"""
+
+import TikTokApi as tta
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import tensorflow as tf
