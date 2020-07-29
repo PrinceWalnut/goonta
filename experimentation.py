e8632ccbee36a26ab2fac8a699a49d5ec7d4bce7
Rick Hewitt <Rick.Lira.Morgan@gmail.com>
2020-07-28 19:07:51 -0700

    Added initializations and a couple of ideas for functions with docstrings but no implementation

--- experimentation.py
+++ experimentation.py
@@ -4,8 +4,20 @@ This is a file to be used for experimentation purposes ONLY. All actual software
 
 """
 
-import TikTokApi as tta
+from TikTokApi import TikTokApi
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import tensorflow as tf
+
+# Initialize some values
+api = TikTokApi()
+puke = api.getUser(username='slowpuke')
+uid = puke.get('userInfo').get('user').get('id')
+
+def get_users_creator_suggested_to(num_users)
+    # Get a list of users that the creator is suggested to
+
+def get_correlated_music(num_sounds)
+    # Get TikTok sounds most suggested to users that the creator is suggested to
+    
